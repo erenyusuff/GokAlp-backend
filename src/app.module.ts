2343586548c9b36f77de40b3d17388c8e0e9787b
Yusuf Eren <yusuf.er48en@gmail.com>
2023-12-29 22:53:35 +0300

    Controllers, services, modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HomepageController } from './homepage/homepage.controller';
+import { HomepageService } from './homepage/homepage.service';
+import { HomepageModule } from './homepage/homepage.module';
+import { AboutController } from './about/about.controller';
+import { AboutService } from './about/about.service';
+import { AboutModule } from './about/about.module';
+import { ProjectsController } from './projects/projects.controller';
+import { ProjectsService } from './projects/projects.service';
+import { ProjectsModule } from './projects/projects.module';
+import { GaleriController } from './galeri/galeri.controller';
+import { ArchiveController } from './archive/archive.controller';
+import { ArchiveService } from './archive/archive.service';
+import { ArchiveModule } from './archive/archive.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HomepageModule, AboutModule, ProjectsModule, ArchiveModule],
+  controllers: [AppController, HomepageController, AboutController, ProjectsController, GaleriController, ArchiveController],
+  providers: [AppService, HomepageService, AboutService, ProjectsService, ArchiveService],
 })
 export class AppModule {}
